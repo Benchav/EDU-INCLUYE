5698d7cbd621dd2d60e745105c3be9b5f9708cd7
benchav <joshua44benja@gmail.com>
2025-11-08 19:38:26 -0600

    remove

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,8 @@ export default function Footer() {
         </div>
         
         <div className="footer__links">
-          <h4 className="footer__heading">Contenido</h4>
+    {/*    <h4 className="footer__heading">Contenido</h4>*/}
+     <h4 className="footer__heading">Enlaces</h4>
           <ul className="footer__link-list">
             <li><a href="/glosario">Categorias</a></li>
             <li><a href="/curso">Cursos</a></li>
@@ -25,30 +26,30 @@ export default function Footer() {
           </ul>
         </div>
         
-        <div className="footer__links">
+     { /*  <div className="footer__links">
           <h4 className="footer__heading">Enlaces</h4>
           <ul className="footer__link-list">
             <li><a href="/acerca">Acerca de Nosotros</a></li>
-            <li><a href="/contacto">Contacto</a></li>
+            <li><a href="/contacto">Contacto</a></li> 
           </ul>
-        </div>
+        </div> */}
         
-        <div className="footer__newsletter">
+    {/*    <div className="footer__newsletter">
           <h4 className="footer__heading">Contacto</h4>
           <p>Recibe las últimas actualizaciones y recursos.</p>
           <form className="footer__form">
             <input type="email" placeholder="Tu correo electrónico" required />
             <button type="submit">Contactanos</button>
           </form>
-        </div>
+        </div>*/}
       </div>
       
-      <div className="footer__socials">
+   { /*  <div className="footer__socials">
         <a href="#" aria-label="Facebook"><FaFacebookF /></a>
         <a href="#" aria-label="Instagram"><FaInstagram /></a>
         <a href="#" aria-label="Twitter"><FaTwitter /></a>
         <a href="#" aria-label="YouTube"><FaYoutube /></a>
-      </div>
+      </div> */}
       
       <div className="footer__bottom">
         <p>© {new Date().getFullYear()} EDU-Incluye. Todos los derechos reservados.</p>
