7bb9fe279a35834f367cc5fac4fa3e587ca38e66
benchav <joshua44benja@gmail.com>
2025-11-22 14:52:21 -0600

    add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,7 +48,7 @@ export default function Login() {
           {error && <div className="login-error">{error}</div>}
 
           <div className="input-group">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Correo</label>
             <div className="input-with-icon">
               <input
                 type="email"
@@ -56,7 +56,7 @@ export default function Login() {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                placeholder="tu@email.com"
+                placeholder="correo@gmail.com"
               />
               <svg className="input-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
